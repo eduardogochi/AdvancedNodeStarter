05719f9d691e9c375859b98389de2fe18c424928
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-15 14:13:31 -0600

    Created test to validate the user is signed in

--- tests/header.test.js
+++ tests/header.test.js
@@ -28,3 +28,47 @@ test('Clicking login starts oAuth flow', async () => {
 
   expect(url).toMatch(/accounts\.google\.com/)
 })
+
+test('When signed in, shows logout button', async () => {
+  /* Steps to fake the session: 
+        1. Create page instance
+        2. Take an existing user Id and generate fake session object with it
+        3. Sign the session object with keygrip
+        4. Set the session and signature on our page instance as cookies
+     */
+
+  const id = '65a0450077383e409169584f'
+
+  const Buffer = require('safe-buffer').Buffer
+  const sessionObj = {
+    passport: {
+      user: id,
+    },
+  }
+
+  const sessionStr = Buffer.from(JSON.stringify(sessionObj)).toString('base64')
+
+  const Keygrip = require('keygrip')
+  const keys = require('../config/keys')
+  const keygrip = new Keygrip([keys.cookieKey])
+  const sig = keygrip.sign('session=' + sessionStr)
+
+  // console.log(sessionStr, sig)
+
+  await page.setCookie({
+    name: 'session',
+    value: sessionStr,
+  })
+
+  await page.setCookie({
+    name: 'session.sig',
+    value: sig,
+  })
+
+  await page.goto('localhost:3000')
+  await page.waitFor('a[href="/auth/logout"]')
+
+  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML)
+
+  expect(text).toEqual('Logout')
+})
