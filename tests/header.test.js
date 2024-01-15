e05af493575801df301414d0151171fc233baf63
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-15 14:49:50 -0600

    Created session and user factories, and jest setup file

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer')
+const sessionFactory = require('./factories/sessionFactory')
+const userFactory = require('./factories/userFactory')
 
 let browser, page
 
@@ -24,40 +26,25 @@ test('Clicking login starts oAuth flow', async () => {
   await page.click('.right a')
   const url = await page.url()
 
-  console.log(url)
-
   expect(url).toMatch(/accounts\.google\.com/)
 })
 
 test('When signed in, shows logout button', async () => {
-  /* Steps to fake the session: 
+  /* 
+  Steps to fake the session: 
         1. Create page instance
         2. Take an existing user Id and generate fake session object with it
         3. Sign the session object with keygrip
         4. Set the session and signature on our page instance as cookies
      */
 
-  const id = '65a0450077383e409169584f'
+  const user = await userFactory()
 
-  const Buffer = require('safe-buffer').Buffer
-  const sessionObj = {
-    passport: {
-      user: id,
-    },
-  }
-
-  const sessionStr = Buffer.from(JSON.stringify(sessionObj)).toString('base64')
-
-  const Keygrip = require('keygrip')
-  const keys = require('../config/keys')
-  const keygrip = new Keygrip([keys.cookieKey])
-  const sig = keygrip.sign('session=' + sessionStr)
-
-  // console.log(sessionStr, sig)
+  const { session, sig } = sessionFactory(user)
 
   await page.setCookie({
     name: 'session',
-    value: sessionStr,
+    value: session,
   })
 
   await page.setCookie({
