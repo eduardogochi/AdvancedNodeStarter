a81fbc0b0744ee65354800e9a1b7f1234bde3d9a
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-17 11:56:41 -0600

    Completed tests for not logged in user

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -59,3 +59,34 @@ describe('When logged in', async () => {
     })
   })
 })
+
+describe('When not logged in', async () => {
+  test('User cannot create blog posts', async () => {
+    const result = await page
+      .evaluate(() => {
+        return fetch('/api/blogs', {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ title: 'My Title', content: 'My content' }),
+        })
+      })
+      .then((res) => res.json())
+
+    expect(result).toEqual({ error: 'You must log in!' })
+  })
+
+  test('User cannot get blog posts', async () => {
+    const result = await page
+      .evaluate(() => {
+        return fetch('/api/blogs', {
+          method: 'GET',
+          credentials: 'same-origin',
+          headers: { 'Content-Type': 'application/json' },
+        })
+      })
+      .then((res) => res.json())
+
+    expect(result).toEqual({ error: 'You must log in!' })
+  })
+})
