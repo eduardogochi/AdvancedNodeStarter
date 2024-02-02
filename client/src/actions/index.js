2a2c10cfe9f1fc650cf6aac1fb6b14d3ba53d865
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-02-02 13:08:01 -0600

    Fixed file upload bug in submitBlog action and uploadRoutes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,8 +16,8 @@ export const handleToken = (token) => async (dispatch) => {
 export const submitBlog = (values, file, history) => async (dispatch) => {
   const uploadConfig = await axios.get('/api/upload')
 
-  await axios.put(uploadConfig.data.url, file, {
-    headers: { 'Content-Type': file.type },
+  await axios.put(uploadConfig.data.url, file[0], {
+    headers: { 'Content-Type': file[0].type },
   })
 
   const res = await axios.post('/api/blogs', {
